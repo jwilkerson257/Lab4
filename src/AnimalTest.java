d0feb049b653d9fdb470ddef205284951d7722df
Josh <Jwilkerson@ou.edu>
2019-02-11 20:56:41 -0600

    Finished Animal Test class

--- src/AnimalTest.java
+++ src/AnimalTest.java
@@ -34,6 +34,36 @@ public class AnimalTest
         Assert.assertEquals(200.0, animal.getHeight(), 0.01);
         Assert.assertEquals(100.0, animal.getWeight(), 0.01);
     }
+    
+    public void getColorTest() throws AssertException
+    {
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+    	Assert.assertEquals("Blue", animal.getColor());
+    }
+    
+    public void getNameTest() throws AssertException
+    {
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+    	Assert.assertEquals("Whale", animal.getName());
+    }
+    
+    public void getHeightTest() throws AssertException
+    {
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+    	Assert.assertEquals(200.0, animal.getHeight());
+    }
+    
+    public void getWeightTest() throws AssertException
+    {
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+    	Assert.assertEquals(100.0, animal.getWeight());
+    }
+    
+    public void toStringTest() throws AssertException
+    {
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+    	Assert.assertEquals("Whale, a Blue-colored animal. 100.0 pounds, 200.0 inches\n", animal.toString());
+    }
 
 }
 
