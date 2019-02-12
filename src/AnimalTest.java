7c4f452650fbbc383ff4084d0933a2c8fd2765f9
Josh <Jwilkerson@ou.edu>
2019-02-11 21:53:05 -0600

    Changed Height variable

--- src/AnimalTest.java
+++ src/AnimalTest.java
@@ -26,43 +26,43 @@ public class AnimalTest
     public void fullConstructorTest() throws AssertException
     {
         // Use the default constructor
-        Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+        Animal animal = new Animal("Blue", "Whale", 100.0, 100.0);
 
         // The name should be "noname", color "unknown", height and weight 0.
         Assert.assertEquals("Blue", animal.getColor());
         Assert.assertEquals("Whale", animal.getName());
-        Assert.assertEquals(200.0, animal.getHeight(), 0.01);
+        Assert.assertEquals(100.0, animal.getHeight(), 0.01);
         Assert.assertEquals(100.0, animal.getWeight(), 0.01);
     }
     
     public void getColorTest() throws AssertException
     {
-    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 100.0);
     	Assert.assertEquals("Blue", animal.getColor());
     }
     
     public void getNameTest() throws AssertException
     {
-    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 100.0);
     	Assert.assertEquals("Whale", animal.getName());
     }
     
     public void getHeightTest() throws AssertException
     {
-    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
-    	Assert.assertEquals(200.0, animal.getHeight());
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 100.0);
+    	Assert.assertEquals(100.0, animal.getHeight());
     }
     
     public void getWeightTest() throws AssertException
     {
-    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 100.0);
     	Assert.assertEquals(100.0, animal.getWeight());
     }
     
     public void toStringTest() throws AssertException
     {
-    	Animal animal = new Animal("Blue", "Whale", 100.0, 200.0);
-    	Assert.assertEquals("Whale, a Blue-colored animal. 100.0 pounds, 200.0 inches\n", animal.toString());
+    	Animal animal = new Animal("Blue", "Whale", 100.0, 100.0);
+    	Assert.assertEquals("Whale, a Blue-colored animal. 100.0 pounds, 100.0 inches\n", animal.toString());
     }
 
 }
